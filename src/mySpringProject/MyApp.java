e7216cfae394af237f8bdff988acedc6cab6ca39
vcsprg6 <vcs-prg6@supportgov.in>
2025-02-22 11:05:49 +0530

    Added the XML configuration for the Object Creation of Address class and the same configuration has been read in Myapp class by using ApplicationContext interface and its implementation class FileSystemXmlApplicationContext.

--- src/mySpringProject/MyApp.java
+++ src/mySpringProject/MyApp.java
@@ -1,5 +1,29 @@
 package mySpringProject;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import mySpringProject.Utility.Address;
+
 public class MyApp {
 
+	public static void main(String[] args) {
+
+		/*
+		 * ApplicationContext is a interface and its implementation class both are use
+		 * to Read the XML file form its location and as its configuration its return
+		 * the object for the given class
+		 */
+		ApplicationContext myContainer = new FileSystemXmlApplicationContext(
+				"D:\\MySpringProject\\MySpringProject\\spring.xml");
+
+		
+		
+		Object myObj = myContainer.getBean("address1");
+
+		/* type cast myObj to our Address classs Type */
+
+		Address a = (Address) myObj;
+
+	}
 }
