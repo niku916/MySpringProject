69f1b7068c00c1190cd8676b234ba8d81c52bd6b
vcsprg6 <vcs-prg6@supportgov.in>
2025-02-22 10:51:34 +0530

    Added to utlity class for example

---
+++ src/mySpringProject/Utility/Address.java
@@ -0,0 +1,24 @@
+package mySpringProject.Utility;
+
+public class Address {
+	
+	private String location;
+	private int pincode;
+	
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	
+	
+
+}
---
+++ src/mySpringProject/Utility/Student.java
@@ -0,0 +1,24 @@
+package mySpringProject.Utility;
+
+public class Student {
+
+	
+	private String name;
+	private int mobileNo;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	
+}
